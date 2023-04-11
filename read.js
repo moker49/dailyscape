536f77b7004d4fee3d44c0936b80bef13dde5561
moker49 <moker49@gmail.com>
2023-04-11 09:20:38 -0400

    data

---
+++ read.js
@@ -0,0 +1,6 @@
+rsapidata["1607"] = {"name": "Cut Saphire","last": 447,"price": 447,}
+rsapidata["1617"] = {"name": "Uncut Diamond","last": 853,"price": 853,}
+rsapidata["1619"] = {"name": "Uncut Ruby","last": 416,"price": 416,}
+rsapidata["1621"] = {"name": "Uncut Emerald","last": 379,"price": 379,}
+rsapidata["1623"] = {"name": "Uncut Saphire","last": 531,"price": 531,}
+rsapidata["3420"] = {"name": "Limestone Brick","last": 1788,"price": 1788,}
--- rsdata/read.js
+++ rsdata/read.js
@@ -1,6 +1,6 @@
-rsapidata["1607"] = {"name": "Cut Saphire","last": 0,"price": 433,}
-rsapidata["1617"] = {"name": "Uncut Diamond","last": 0,"price": 922,}
-rsapidata["1619"] = {"name": "Uncut Ruby","last": 0,"price": 382,}
-rsapidata["1621"] = {"name": "Uncut Emerald","last": 0,"price": 321,}
-rsapidata["1623"] = {"name": "Uncut Saphire","last": 0,"price": 512,}
-rsapidata["3420"] = {"name": "Limestone Brick","last": 0,"price": 1783,}
+rsapidata["1607"] = {"name": "Cut Saphire","last": 419,"price": 447,}
+rsapidata["1617"] = {"name": "Uncut Diamond","last": 933,"price": 853,}
+rsapidata["1619"] = {"name": "Uncut Ruby","last": 387,"price": 416,}
+rsapidata["1621"] = {"name": "Uncut Emerald","last": 344,"price": 379,}
+rsapidata["1623"] = {"name": "Uncut Saphire","last": 521,"price": 531,}
+rsapidata["3420"] = {"name": "Limestone Brick","last": 1463,"price": 1788,}
